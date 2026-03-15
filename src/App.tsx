ce5b33d0f35474e6fc89ca1a36972ef5bac619f1
“guiherme” <guilherme@agapesistemas.com.br>
2026-03-14 21:32:14 -0300

    fix(initial): resume code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { isEmbedMode } from '@/app/utils/is-embed-mode';
 import { Router } from '@/routes';
-import { TooltipProvider } from '@/views/components/ui/tooltip';
 import { Widget } from '@/views/pages/widget';
 
 export function App() {
@@ -8,9 +7,5 @@ export function App() {
     return <Widget />;
   }
 
-  return (
-    <TooltipProvider>
-      <Router />
-    </TooltipProvider>
-  );
+  return <Router />;
 }
